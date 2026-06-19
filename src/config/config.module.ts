e3cd543210dbe51680f3b0ca733d5a31a8c167d0
Pratchaya007 <pratchaya.1012@gmail.com>
2026-06-19 15:48:31 +0700

    chore: cofig swagger

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { TypedConfigService } from './typed-config.service';
+import { validate } from './env.validation';
 
 @Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validate: validate,
+    }),
+  ],
   providers: [TypedConfigService],
   exports: [TypedConfigService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalValidationPip } from './common/pipes/global-validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new GlobalValidationPip());
 
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Nest Auth Login')
+    .setDescription('Complete Authorization system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  const port = configService.get<number>('PORT') ?? 8000;
+
+  await app.listen(port);
+
+  console.log(`Application running on http://localhost:${port}/api`);
+  console.log(`Swagger docs at http://localhost:${port}/api/docs`);
 }
 bootstrap();
